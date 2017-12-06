09a91b0a25d2b9652761d5f3b48aa1a73260a59a
chetans_quadlogix <chetans_quadlogix>
2017-12-06 15:42:10 +0530

    added simple test casesto test it on jenkins locally

---
+++ app/src/test/java/com/example/rac/sampleapitesting/LoginPresenterTest.java
@@ -0,0 +1,28 @@
+package com.example.rac.sampleapitesting;
+
+import android.util.Log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class LoginPresenterTest {
+
+    public static String TAG  = "LOGINPRESENTERTEST";
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void substraction() throws Exception
+    {
+        assertEquals("EXPECTED 4",4,4 );
+    }
+}
